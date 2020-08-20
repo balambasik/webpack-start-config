736cf1325c04e829ec34aae59884d453bfb9b81c
Deniel <balambasik@gmail.com>
2020-08-20 11:24:07 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Class from "./js/Class";
 import "./css/style.css";
+import "./less/style.less"
 
 console.log('---', (new Class()).foo());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
     entry: './src/index.js',
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [(process.env.NODE_ENV === "production" ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader']
+                test: /\.(css|less)$/,
+                use: [(process.env.NODE_ENV === "production" ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'less-loader']
             },
             {test: /\.(js)$/, use: 'babel-loader'}
         ]
@@ -18,10 +20,19 @@ module.exports = {
         filename: '[name].[contenthash].js'
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css'
         }),
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery/dist/jquery.min.js",
+            jQuery: "jquery/dist/jquery.min.js",
+            "window.jQuery": "jquery/dist/jquery.min.js"
+        })
     ],
     mode: process.env.NODE_ENV === "production" ? "production" : "development",
     devServer: {
